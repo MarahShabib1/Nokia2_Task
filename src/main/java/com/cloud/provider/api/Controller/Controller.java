7ba96e2bbc9ef1ce07a947acc186a2d127175e1f
Marah Shabib <marahshabib@hotmail.com>
2021-02-18 16:01:03 +0200

    faster4

--- src/main/java/com/cloud/provider/api/Controller/Controller.java
+++ src/main/java/com/cloud/provider/api/Controller/Controller.java
@@ -26,11 +26,11 @@ public class Controller {
 	@Autowired
 	public ServerService serverService;
 
-	@GetMapping("create")
+	/*@GetMapping("create")
 	public Server createServer() {
 
 		return serverService.createServer(10);
-	}
+	}*/
 
 	@GetMapping("servers")
 	public List<Server> getAllServers() {
